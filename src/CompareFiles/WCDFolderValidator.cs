5df8131be9673cc1ee07c00493039bcc5fdc37fc
Sunil P <ironpython2001@gmail.com>
2025-04-01 17:02:10 +0530

    un

--- src/CompareFiles/WCDFolderValidator.cs
+++ src/CompareFiles/WCDFolderValidator.cs
@@ -11,62 +11,67 @@ public class WCDFolderValidator : IWCDFolderValidator
         string wcdDirPath = Path.Combine(srcDir, wcdFileId);
         string wcdZipFile = Path.Combine(wcdDirPath, $"{wcdFileId}.zip");
         string wcdMD5File = Path.Combine(wcdDirPath, $"{wcdFileId}.md5");
-        
-        if (!File.Exists(wcdZipFile))
+        try
         {
-            return $"{wcdFileId}.zip file does not exist.";
-        }
-        if (!File.Exists(wcdMD5File))
-        {
-            return $"{wcdFileId}.md5 file does not exist.";
-        }
-
-        Dictionary<string, string> filesToCheck = new()
-        {
-            ["media/kestra-pfs/data/database/primary.db"] = string.Empty,
-            ["media/kestra-pfs/data/database/primary.db-shm"] = string.Empty,
-            ["media/kestra-pfs/data/database/primary.db-wal"] = string.Empty
-        };
-
-        await foreach (string line in File.ReadLinesAsync(wcdMD5File))
-        {
-            string[] parts = line.Split("  ", StringSplitOptions.RemoveEmptyEntries);
-            if (parts.Length == 2 && filesToCheck.ContainsKey(parts[1]))
+            if (!File.Exists(wcdZipFile))
             {
-                filesToCheck[parts[1]] = parts[0];
-                if (!filesToCheck.ContainsValue(string.Empty)) break;
+                return $"{wcdFileId}.zip file does not exist.";
             }
-        }
-
-        StringBuilder result = new();
-        int matchCount = 0;
-        using (FileStream zipToOpen = new(wcdZipFile, FileMode.Open, FileAccess.Read, FileShare.Read))
-        using (ZipArchive archive = new(zipToOpen, ZipArchiveMode.Read))
-        {
-            foreach (ZipArchiveEntry entry in archive.Entries)
+            if (!File.Exists(wcdMD5File))
             {
-                if (filesToCheck.ContainsKey(entry.FullName))
+                return $"{wcdFileId}.md5 file does not exist.";
+            }
+
+            Dictionary<string, string> filesToCheck = new()
+            {
+                ["media/kestra-pfs/data/database/primary.db"] = string.Empty,
+                ["media/kestra-pfs/data/database/primary.db-shm"] = string.Empty,
+                ["media/kestra-pfs/data/database/primary.db-wal"] = string.Empty
+            };
+
+            await foreach (string line in File.ReadLinesAsync(wcdMD5File))
+            {
+                string[] parts = line.Split("  ", StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 2 && filesToCheck.ContainsKey(parts[1]))
                 {
-                    using Stream zipStream = entry.Open();
-                    byte[] hashResult = await MD5.HashDataAsync(zipStream);
-                    string computedChecksum = Convert.ToHexString(hashResult).ToLowerInvariant();
-
-                    if (filesToCheck[entry.FullName] == computedChecksum)
-                    {
-                        result.AppendLine($"{entry.FullName} - MATCH");
-                        matchCount++;
-                    }
-                    else
-                    {
-                        result.AppendLine($"{entry.FullName} - MISMATCH");
-                    }
-
-                    if (matchCount == filesToCheck.Count) break;
+                    filesToCheck[parts[1]] = parts[0];
+                    if (!filesToCheck.ContainsValue(string.Empty)) break;
                 }
             }
+
+            StringBuilder result = new();
+            int matchCount = 0;
+            using (FileStream zipToOpen = new(wcdZipFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (ZipArchive archive = new(zipToOpen, ZipArchiveMode.Read))
+            {
+                foreach (ZipArchiveEntry entry in archive.Entries)
+                {
+                    if (filesToCheck.ContainsKey(entry.FullName))
+                    {
+                        using Stream zipStream = entry.Open();
+                        byte[] hashResult = await MD5.HashDataAsync(zipStream);
+                        string computedChecksum = Convert.ToHexString(hashResult).ToLowerInvariant();
+
+                        if (filesToCheck[entry.FullName] == computedChecksum)
+                        {
+                            result.AppendLine($"{entry.FullName} - MATCH");
+                            matchCount++;
+                        }
+                        else
+                        {
+                            result.AppendLine($"{entry.FullName} - MISMATCH");
+                        }
+
+                        if (matchCount == filesToCheck.Count) break;
+                    }
+                }
+            }
+            return result.ToString();
         }
-        return result.ToString();
-        
+        catch (Exception ex)
+        {
+            return $"{srcDir},{wcdFileId},{Environment.NewLine} exception message:{ex.Message},{Environment.NewLine}stacktrace: {ex.StackTrace}";
+        }
     }
 }
 
