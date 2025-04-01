2ecc44ca1ae29b704c9bcb293b4b181620e7ab91
Sunil P <ironpython2001@gmail.com>
2025-04-01 20:39:25 +0530

    updated code to .net 6.0

--- src/CompareFiles/Program.cs
+++ src/CompareFiles/Program.cs
@@ -2,7 +2,7 @@
 //https://code-maze.com/csharp-compare-two-files/#:~:text=If%20the%20file%20sizes%20are%20the%20same%2C%20we%20create%20a,the%20same%20name%20and%20size.
 using System.Net;
 
-var wcdFileId = "00006722_1697493954000";
+var wcdFileId = "00007794_1711405171000";//"00006722_1697493954000";
 var srcDir = @"C:\Users\spottumuttu\Downloads";
 
 Console.WriteLine($"Start time: {DateTime.Now}");
--- src/CompareFiles/WCDFolderValidator.cs
+++ src/CompareFiles/WCDFolderValidator.cs
@@ -1,5 +1,8 @@
 ﻿//References
 //https://code-maze.com/csharp-compare-two-files/#:~:text=If%20the%20file%20sizes%20are%20the%20same%2C%20we%20create%20a,the%20same%20name%20and%20size.
+//https://stackoverflow.com/questions/10520048/calculate-md5-checksum-for-a-file
+//https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/preprocessor-directives
+using System.IO;
 using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
@@ -22,13 +25,13 @@ public class WCDFolderValidator : IWCDFolderValidator
                 return $"{wcdFileId}.md5 file does not exist.";
             }
 
-            Dictionary<string, string> filesToCheck = new()
+            Dictionary<string, string> filesToCheck = new Dictionary<string, string>()
             {
                 ["media/kestra-pfs/data/database/primary.db"] = string.Empty,
                 ["media/kestra-pfs/data/database/primary.db-shm"] = string.Empty,
                 ["media/kestra-pfs/data/database/primary.db-wal"] = string.Empty
             };
-
+#if NET9_0_OR_GREATER
             await foreach (string line in File.ReadLinesAsync(wcdMD5File))
             {
                 string[] parts = line.Split("  ", StringSplitOptions.RemoveEmptyEntries);
@@ -38,7 +41,18 @@ public class WCDFolderValidator : IWCDFolderValidator
                     if (!filesToCheck.ContainsValue(string.Empty)) break;
                 }
             }
-
+#endif
+#if NET6_0
+            foreach (string line in File.ReadLines(wcdMD5File))
+            {
+                string[] parts = line.Split("  ", StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 2 && filesToCheck.ContainsKey(parts[1]))
+                {
+                    filesToCheck[parts[1]] = parts[0];
+                    if (!filesToCheck.ContainsValue(string.Empty)) break;
+                }
+            }
+#endif
             StringBuilder result = new();
             int matchCount = 0;
             using (FileStream zipToOpen = new(wcdZipFile, FileMode.Open, FileAccess.Read, FileShare.Read))
@@ -48,6 +62,7 @@ public class WCDFolderValidator : IWCDFolderValidator
                 {
                     if (filesToCheck.ContainsKey(entry.FullName))
                     {
+#if NET9_0_OR_GREATER
                         using Stream zipStream = entry.Open();
                         byte[] hashResult = await MD5.HashDataAsync(zipStream);
                         string computedChecksum = Convert.ToHexString(hashResult).ToLowerInvariant();
@@ -63,6 +78,24 @@ public class WCDFolderValidator : IWCDFolderValidator
                         }
 
                         if (matchCount == filesToCheck.Count) break;
+#endif
+#if NET6_0
+                        using var md5 = MD5.Create();
+                        using Stream zipStream = entry.Open();
+                        var hashResult = await md5.ComputeHashAsync(zipStream);
+                        string computedChecksum = Convert.ToHexString(hashResult).ToLowerInvariant();
+                        if (filesToCheck[entry.FullName] == computedChecksum)
+                        {
+                            result.AppendLine($"{entry.FullName} - MATCH");
+                            matchCount++;
+                        }
+                        else
+                        {
+                            result.AppendLine($"{entry.FullName} - MISMATCH");
+                        }
+
+                        if (matchCount == filesToCheck.Count) break;
+#endif
                     }
                 }
             }
